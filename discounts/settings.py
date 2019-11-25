8e6af6d0092187f07c0e42e6110529c66f660cd3
Basil Dubyk <samitnuk.work@gmail.com>
2019-11-25 19:38:52 +0200

    Add `SITE_ID` to project settings

--- discounts/settings.py
+++ discounts/settings.py
@@ -81,6 +81,8 @@ INSTALLED_APPS = [
     'django_tables2',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
