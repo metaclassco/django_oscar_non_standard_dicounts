97c19e14061e84527b67b8d7c66b5c91362ea8fa
Basil Dubyk <samitnuk.work@gmail.com>
2019-11-29 18:03:34 +0200

    Add `bonuses` field to `User` model

--- core/models.py
+++ core/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class User(AbstractUser):
     birthday = models.DateField(null=True)
     is_affiliate_discount_used = models.BooleanField(default=False)
+    bonuses = models.PositiveSmallIntegerField(default=0)
