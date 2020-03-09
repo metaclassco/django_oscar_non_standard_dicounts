51411f2643a97862302b9f7531371727294d40fc
Alexander Gaevsky <sasha@sasha0.ru>
2020-03-09 21:18:25 +0200

    Rework affiliate discounts record in the order creator, make it more DRY.

--- apps/order/utils.py
+++ apps/order/utils.py
@@ -1,13 +1,8 @@
-from decimal import Decimal as D
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.db import transaction
 from django.db.models import F
-from django.utils.translation import gettext_lazy as _
 
-from oscar.apps.order.signals import order_placed
-from oscar.apps.order.utils import OrderCreator as OriginalOrderCreator, OrderNumberGenerator
+from oscar.apps.order.utils import OrderCreator as OriginalOrderCreator
 from oscar.core.loading import get_model
 
 
@@ -16,78 +11,26 @@ Order = get_model('order', 'Order')
 
 
 class OrderCreator(OriginalOrderCreator):
+    user = None
+    request = None
 
-    def place_order(self, basket, total,  # noqa (too complex (12))
-                    shipping_method, shipping_charge, user=None,
-                    shipping_address=None, billing_address=None,
-                    order_number=None, status=None, request=None, **kwargs):
-        """
-        Placing an order involves creating all the relevant models based on the
-        basket and session data.
-        """
-        if basket.is_empty:
-            raise ValueError(_("Empty baskets cannot be submitted"))
-        if not order_number:
-            generator = OrderNumberGenerator()
-            order_number = generator.order_number(basket)
-        if not status and hasattr(settings, 'OSCAR_INITIAL_ORDER_STATUS'):
-            status = getattr(settings, 'OSCAR_INITIAL_ORDER_STATUS')
+    def place_order(self, *args, **kwargs):
+        self.user = kwargs.get('user', None)
+        self.request = kwargs.get('request', None)
+        return super().place_order(*args, **kwargs)
 
-        if Order._default_manager.filter(number=order_number).exists():
-            raise ValueError(_("There is already an order with number %s") % order_number)
+    def record_discount(self, discount):
+        if discount['offer'].slug == settings.AFFILIATE_OFFER_SLUG:
+            self.record_affiliate_discount_usage()
 
-        with transaction.atomic():
+    def record_affiliate_discount_usage(self):
+        self.user.is_affiliate_discount_used = True
+        self.user.save()
 
-            # Ok - everything seems to be in order, let's place the order
-            order = self.create_order_model(
-                user, basket, shipping_address, shipping_method, shipping_charge,
-                billing_address, total, order_number, status, request, **kwargs)
-            for line in basket.all_lines():
-                self.create_line_models(order, line)
-                self.update_stock_records(line)
-
-            for voucher in basket.vouchers.select_for_update():
-                available_to_user, msg = voucher.is_available_to_user(user=user)
-                if not voucher.is_active() or not available_to_user:
-                    raise ValueError(msg)
-
-            # Record any discounts associated with this order
-            for application in basket.offer_applications:
-                # Trigger any deferred benefits from offers and capture the
-                # resulting message
-                application['message'] = application['offer'].apply_deferred_benefit(basket, order, application)
-                # Record offer application results
-                if application['result'].affects_shipping:
-                    # Skip zero shipping discounts
-                    shipping_discount = shipping_method.discount(basket)
-                    if shipping_discount <= D('0.00'):
-                        continue
-                    # If a shipping offer, we need to grab the actual discount off
-                    # the shipping method instance, which should be wrapped in an
-                    # OfferDiscount instance.
-                    application['discount'] = shipping_discount
-                self.create_discount_model(order, application)
-                self.record_discount(application)
-
-                if application['offer'].slug == settings.AFFILIATE_OFFER_SLUG:
-                    self.record_affiliate_discount_usage(request, user)
-
-            for voucher in basket.vouchers.all():
-                self.record_voucher_usage(order, voucher, user)
-
-        # Send signal for analytics to pick up
-        order_placed.send(sender=self, order=order, user=user)
-
-        return order
-
-    def record_affiliate_discount_usage(self, request, user):
-        user.is_affiliate_discount_used = True
-        user.save()
-
-        if settings.AFFILIATE_SESSION_KEY in request.session.keys():
-            affiliate_username = request.session[settings.AFFILIATE_SESSION_KEY]
+        if settings.AFFILIATE_SESSION_KEY in self.request.session.keys():
+            affiliate_username = self.request.session[settings.AFFILIATE_SESSION_KEY]
             affiliate = User.objects.get(username=affiliate_username)
             affiliate.bonuses = F('bonuses') + settings.AFFILIATE_BONUSES
             affiliate.save()
 
-            del request.session[settings.AFFILIATE_SESSION_KEY]
+            del self.request.session[settings.AFFILIATE_SESSION_KEY]
