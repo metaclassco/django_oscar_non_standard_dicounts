e4d744610f4e3d30a62fc50c90c66ea279d31f20
Alexander Gaevsky <sasha@sasha0.ru>
2020-03-09 16:09:45 +0200

    Cleanup obsolete core application.

--- core/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    birthday = models.DateField(null=True)
-    is_affiliate_discount_used = models.BooleanField(default=False)
-    bonuses = models.PositiveSmallIntegerField(default=0)
