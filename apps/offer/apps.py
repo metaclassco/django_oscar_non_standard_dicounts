c761007bd645257e96fb8675d67bde9514322ffd
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-24 17:19:28 +0300

    Rework referral code application, implement code validation.

--- apps/offer/apps.py
+++ apps/offer/apps.py
@@ -1,20 +1,15 @@
 from django.conf.urls import url
 
 import oscar.apps.offer.apps as apps
-from oscar.core.loading import get_class
+
+from .views import SetReferralCodeAndRedirectView
 
 
 class OfferConfig(apps.OfferConfig):
     name = 'apps.offer'
 
-    def ready(self):
-        super().ready()
-
-        self.get_discount_view = get_class('offer.views', 'SetAffiliateAndRedirectView')
-
     def get_urls(self):
         urls = [
-            url(r'^get-discount/(?P<username>[\w-]+)/$', self.get_discount_view.as_view(),
-                name='get-discount'),
+            url(r'^rp/(?P<referral_code>[\w-]+)$', SetReferralCodeAndRedirectView.as_view(), name='referral_program'),
         ]
         return super().get_urls() + self.post_process_urls(urls)
--- apps/offer/views.py
+++ apps/offer/views.py
@@ -8,22 +8,17 @@ from django.views.generic.base import RedirectView
 User = get_user_model()
 
 
-class SetAffiliateAndRedirectView(RedirectView):
+class SetReferralCodeAndRedirectView(RedirectView):
     url = reverse_lazy('catalogue:index')
 
     def get(self, request, *args, **kwargs):
-        self.set_affiliate_in_session(request)
+        referral_code = self.kwargs.get('referral_code', None)
+        if referral_code:
+            if User.objects.filter(referral_code=referral_code).exists():
+                request.session[settings.AFFILIATE_SESSION_KEY] = referral_code
+                messages.success(request, "Your referral code is valid, discount will be applied on checkout.")
+            else:
+                messages.error(request, "Referrer account not found.")
+        else:
+            messages.error(request, "Referral code is not provided.")
         return super().get(request, *args, **kwargs)
-
-    def set_affiliate_in_session(self, request):
-        affiliate = self.kwargs['username']
-        if User.objects.filter(username=affiliate).exists():
-            affiliate_user = User.objects.get(username=affiliate)
-            user = request.user
-            if affiliate_user != user and not user.is_affiliate_discount_used:
-                request.session[settings.AFFILIATE_SESSION_KEY] = affiliate
-                message = (
-                    'You have discount from {}. '
-                    'You will see it in your basket.'.format(affiliate_user.get_full_name())
-                )
-                messages.add_message(request, messages.SUCCESS, message)
