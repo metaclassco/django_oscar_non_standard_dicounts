82a53e8caf7cdc17af361151aa0fe179947f7f76
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-24 15:53:33 +0300

    Give user model field for indicating referral code usage more accurate name.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class User(AbstractUser):
     date_of_birth = models.DateField(null=True)
-    is_affiliate_discount_used = models.BooleanField(default=False)
+    is_referral_code_used = models.BooleanField(default=False)
     bonuses = models.PositiveSmallIntegerField(default=0)
