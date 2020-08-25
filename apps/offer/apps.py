fb2698ba94d63b4309e27a0c59d12fda66510bc0
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-25 09:42:52 +0300

    Prevent circular imports in offer application config, update name for referral program view URLconf.

--- apps/offer/apps.py
+++ apps/offer/apps.py
@@ -2,14 +2,15 @@ from django.conf.urls import url
 
 import oscar.apps.offer.apps as apps
 
-from .views import SetReferralCodeAndRedirectView
-
 
 class OfferConfig(apps.OfferConfig):
     name = 'apps.offer'
 
     def get_urls(self):
+        from .views import SetReferralCodeAndRedirectView
+
         urls = [
-            url(r'^rp/(?P<referral_code>[\w-]+)$', SetReferralCodeAndRedirectView.as_view(), name='referral_program'),
+            url(r'^rp/(?P<referral_code>[\w-]+)$', SetReferralCodeAndRedirectView.as_view(),
+                name='apply_referral_code'),
         ]
         return super().get_urls() + self.post_process_urls(urls)
