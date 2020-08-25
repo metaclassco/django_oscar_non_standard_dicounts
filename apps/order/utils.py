9be08e8619ddf363eb65bfffa4868372c763d6ba
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-25 15:21:06 +0300

    Track referrer when recording discount during order placement.

--- apps/order/utils.py
+++ apps/order/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
@@ -5,6 +7,7 @@ from oscar.apps.order.utils import OrderCreator as OriginalOrderCreator
 from oscar.core.loading import get_model
 
 
+logger = logging.getLogger(__file__)
 User = get_user_model()
 Order = get_model('order', 'Order')
 
@@ -21,8 +24,17 @@ class OrderCreator(OriginalOrderCreator):
     def record_discount(self, discount):
         super().record_discount(discount)
 
-        if settings.AFFILIATE_SESSION_KEY in self.request.session.keys():
+        referral_code = self.request.session.get(settings.AFFILIATE_SESSION_KEY, None)
+
+        if referral_code:
+            try:
+                referrer = User.objects.get(referral_code=referral_code)
+            except User.DoesNotExist:
+                referrer = None
+                logger.error("Could not retrieve referrer for referral code '%s'", referrer)
+
             self.user.is_referral_code_used = True
+            self.user.referrer = referrer
             self.user.save()
 
             del self.request.session[settings.AFFILIATE_SESSION_KEY]
