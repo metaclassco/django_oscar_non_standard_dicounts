2959fd377b472bbcddad949ad5c09b1a795d6142
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-25 14:11:32 +0300

    Rework offer application in the applicator class in order to pass request to the benefit application and perform condition validation accondingly.

--- apps/offer/applicator.py
+++ apps/offer/applicator.py
@@ -1,11 +1,15 @@
 from oscar.apps.offer.applicator import Applicator as CoreApplicator
-from oscar.core.loading import get_model
+from oscar.core.loading import get_class, get_model
 
 
 ConditionalOffer = get_model('offer', 'ConditionalOffer')
+OfferApplications = get_class('offer.results', 'OfferApplications')
 
 
 class Applicator(CoreApplicator):
+    def apply(self, basket, user=None, request=None):
+        offers = self.get_offers(basket, user, request)
+        self.apply_offers(basket, offers, request)
 
     def get_user_offers(self, user):
         qs = ConditionalOffer.active.filter(offer_type=ConditionalOffer.USER)
@@ -13,9 +17,24 @@ class Applicator(CoreApplicator):
 
     def get_session_offers(self, request):
         offers = []
-        qs = ConditionalOffer.active.filter(offer_type=ConditionalOffer.USER)
+        qs = ConditionalOffer.active.filter(offer_type=ConditionalOffer.SESSION)
         qs = qs.select_related('condition', 'benefit')
         for offer in qs:
             if offer.is_condition_satisfied(basket=request.basket, request=request):
                 offers.append(offer)
         return offers
+
+    def apply_offers(self, basket, offers, request):
+        applications = OfferApplications()
+        for offer in offers:
+            num_applications = 0
+            while num_applications < offer.get_max_applications(basket.owner):
+                result = offer.apply_benefit(basket, request=request)
+                num_applications += 1
+                if not result.is_successful:
+                    break
+                applications.add(offer, result)
+                if result.is_final:
+                    break
+
+        basket.offer_applications = applications
