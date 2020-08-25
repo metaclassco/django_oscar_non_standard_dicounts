2cfeae560e2d9f254452dad2a87bf1d7fbecb32d
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-25 09:16:15 +0300

    Implement user model property with generated referral link.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.sites.models import Site
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class User(AbstractUser):
@@ -7,3 +9,9 @@ class User(AbstractUser):
     is_referral_code_used = models.BooleanField(default=False)
     bonuses = models.PositiveSmallIntegerField(default=0)
     referral_code = models.CharField(null=True, blank=True, max_length=10, unique=True)
+
+    @property
+    def referral_link(self):
+        current_site = Site.objects.get_current()
+        referral_url = reverse_lazy("offer:apply_referral_code", args=(self.referral_code,))
+        return "http://%s%s" % (current_site.domain, referral_url)
