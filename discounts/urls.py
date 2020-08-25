f3b268ffa5eb6265909cdb0c3bdda701211e47c5
Alexander Gaevsky <sasha@sasha0.ru>
2020-08-25 13:33:13 +0300

    Include media files URLconf to the root URLconf in order to get media files served during development.

--- discounts/urls.py
+++ discounts/urls.py
@@ -1,8 +1,10 @@
 from django.apps import apps
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(apps.get_app_config('oscar').urls[0])),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
